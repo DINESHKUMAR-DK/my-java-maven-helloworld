703191297738bf45992726f87a5d96ca93388c61
Dinesh <jeetdinesh282@gmail.com>
2021-02-03 13:04:58 +0530

    chnage code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,5 @@
 package com.mycompany.app;
+import java.lang.System;
 
 /**
  * Hello world!
@@ -9,5 +10,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        
+        System.out.println( "Hello World!" );
+        String abc="Hello dinesh ";
+        myfunction(abc);
+
+    }
+    static void myfunction(String temp){
+        System.out.println(temp);
     }
 }
